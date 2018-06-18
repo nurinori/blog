f4b0004d82e51475a2b585a33356fee54c32866b
nuri nori <in.joy.nuri@gmail.com>
2018-06-18 21:27:22 +0300

    Удаляет полифил для ИЕ

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,6 @@ gulp.task("scripts", function() {
   return gulp.src([
     "node_modules/jquery/dist/jquery.min.js",
     "node_modules/slick-carousel/slick/slick.min.js",
-    "node_modules/picturefill//dist/picturefill.min.js",
     "source/js/*.js"
     ])
     .pipe(concat("build.js"))
